bab6e917407568e4812bcf913f63543d8c25c681
cazhiareese <46840412+cazhiareese@users.noreply.github.com>
2019-05-15 13:15:34 +0800

    Add files via upload

---
+++ camera_library(edited).py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+# TODO
+# WRITE YOUR CAMERA CAPTURE LIBRARY HERE
+def capture():
+	while(True):
+    	ret, frame = cap.read()
+    	cv2.imshow("frame", frame)
+    	if cv2.waitKey(20) & 0xFF == ord("q"):
+    		break
+	cap.release()
+	cv2.destroyAllWindows()
