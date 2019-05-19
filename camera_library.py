890bbbabb89c593b88a8efd0541ee792e29e671c
cazhiareese <46840412+cazhiareese@users.noreply.github.com>
2019-05-19 11:35:47 +0800

    Update camera_library.py

--- camera_library.py
+++ camera_library.py
@@ -1,6 +1,14 @@
 import cv2
+cap = cv2.VideoCapture(0)
 
 # TODO
 # WRITE YOUR CAMERA CAPTURE LIBRARY HERE
 def capture():
+    while(True):
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(20) & 0xFF == ord("q"):
+    	break
+cap.release()
+cv2.destroyAllWindows()
     return
