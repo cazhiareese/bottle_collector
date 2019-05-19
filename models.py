fc92cde044e3d13fc562ca090c87dded6f2da106
cazhiareese <46840412+cazhiareese@users.noreply.github.com>
2019-05-19 11:43:03 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-
-__all__ = ['User', 'Bottle']
+__all__ = ['User', 'Bottle', 'Location','Transaction']
 db = SQLAlchemy()
 
-
 class User(db.Model):
+	
+	__tablename__ = 'user'
 
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
     address = db.Column(db.String(128), default=None)
     birthday = db.Column(db.Date, default=None)
@@ -20,20 +18,41 @@ class User(db.Model):
         self.birthday = user_obj['birthday']
         self.gender = user_obj['gender']
 
-
-class Bottle(db.Model):
-
+class Bottle(db.Model)
+        
     __tablename__ = 'bottle'
 
-    id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
-    type = db.Column(db.Integer, nullable=False)
+    _type = db.Column(db.Integer, nullable=False)
     points = db.Column(db.Integer, nullable=False)
     
+
     def __init__(self, bottles_obj):
         self.name = bottles_obj['name']
         self._type = bottles_obj['_type']
         self.points = bottles_obj['points']
 
-# TODO
-# Fill out the rest
+class Location(db.Model):
+    __tablename__ = 'location'
+
+	id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String(64), unique=True, nullable=False)
+
+    def __init__(self, location_obj):
+        self.location_name = location_obj['location_name']
+
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
+
+	id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(64), unique=True, nullable=False)
+    location_id = db.Column(db.Integer, default=None)
+  	bottle_type = db.Column(db.Integer, default=None)
+    date = db.Column(db.datetime(6), default=None)
+
+    def __init__(self, transactions_obj):
+        self.user_name = transactions_obj['user_name']
+        self.location_id = transactions_obj['location_id']
+        self.bottle_type = transactions_obj['bottle_type']
+        self.date = transactions_obj['date']
