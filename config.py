22c9984b9c5036c2000f986c6b057121ecdca169
cazhiareese <46840412+cazhiareese@users.noreply.github.com>
2019-05-19 11:41:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 """
 Configuration class
 """
-
+from flask import Flask, flash, redirect, render_template
+app= Flask(__name__)
 class Config():
     # TODO
-    APP_NAME = 'Replace with application name'
+    APP_NAME = 'plastic bottle collector'
 
     # Enable debug mode
     DEBUG = True
@@ -15,7 +16,7 @@ class Config():
         'host': 'localhost',
         'db': 'bottle_collector',
         'user': 'root',
-        'password': '',
+        'password': 'jabelojabelo101',
         'port': 3306
     }
 
@@ -31,5 +32,23 @@ class Config():
 
     # TODO
     # This is where frontend should go, create a route for all UI files
+    @app.route("/page1")
+    def page1():
+        return render_template('page1.html',**locals())
+
+    @app.route("/page2")
+    def page2():
+        return render_template('page2.html',**locals())
+
+    @app.route("/page4")
+    def page4():
+        return render_template('page4.html',**locals())
+
+    @app.route("/page5")
+    def page5():
+        return render_template('page5.html',**locals())
+    if __name__ == "__main__":
+        app.run()
+
     # Setup template folder for webpages
-    TEMPLATE_FOLDER = '/replace/template/folder/here/'
+    TEMPLATE_FOLDER = "C:/Users/cecilia/Documents/programming_practice_l/templates"
